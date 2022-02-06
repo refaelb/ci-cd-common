8d456d0fcf7dbdd6e7f9778b5d7ea2e86612b5ef
refael test <refael058325@gmail.com>
2022-02-06 09:19:35 +0000

    update test cicd common

--- test.sh
+++ test.sh
@@ -1,7 +1,18 @@
-echo Write your commands here
 pwd
-echo Hello world
-
 ls -l
-
-while read i; do echo "$p" ;  helm template common ./common -f $i-values.yaml --output-dir ./template/$i-common ; done <fitcher.txt
+count=0
+while read i
+do 
+    echo "$p" 
+    helm template common ./common -f $i-values.yaml --output-dir ./$i-common 
+    kubectl apply --dry-run=server -f ./$i-common/common/templates/deployment.yaml 
+    if (($? == 0)) ;then echo "work"; count=$((count+1)) ;else echo "bad"; fi
+done <fitcher.txt
+len=`wc -l < fitcher.txt`
+if [ "$count" == "$len" ] 
+then 
+    echo "good" 
+    helm package ./common/
+else  
+    echo "nott"
+fi
