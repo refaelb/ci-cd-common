0442a07d8aa23b23e73e23da178250e71cf32b15
refael test <refael058325@gmail.com>
2022-02-07 07:42:32 +0000

    update script

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 pwd
 ls -l
 count=0
-while read i
-do 
-    echo "$p" 
-    helm template common ./common -f $i-values.yaml --output-dir ./$i-common 
-    kubectl apply --dry-run=server -f ./$i-common/common/templates/deployment.yaml 
+list=0
+for a in "$search_dir"./*yaml; do   echo "$entry"
+    echo $a
+    helm template common ./common -f $a --output-dir ./$a-common 
+    kubectl apply --dry-run=server -f ./$a-common/common/templates/deployment.yaml
     if (($? == 0)) ;then echo "work"; count=$((count+1)) ;else echo "bad"; fi
-done <fitcher.txt
-len=`wc -l < fitcher.txt`
-if [ "$count" == "$len" ] 
+    list=$((list+1))
+done
+if [ "$count" == "$list" ] 
 then 
     echo "good" 
     helm package ./common/
 else  
-    echo "nott"
+    echo "you ave a err"
 fi
